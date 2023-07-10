b12c61e4f3fdc61f71b11e00fb5a35c2f9b1f79e
Unknown807 <milovangveric@gmail.com>
2023-07-10 14:55:48 +0100

    wrote tests for local_store.dart

--- test/project/local_store_test.dart
+++ test/project/local_store_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:recipe_social_media/utilities/utilities.dart';
 import '../fakes/secure_storage_fake.dart';
 
-//class FlutterSecureStorageMock extends Mock implements FlutterSecureStorage {}
-
 void main() {
   late SecureStorageFake secureStorageFake;
   late LocalStore sut; // system under test
@@ -14,26 +12,75 @@ void main() {
   });
 
   group("keyExists method tests", () {
+    test("keyExists returns true", () async {
+      // Arrange
+      await secureStorageFake.write(key: "test-key", value: "test-value");
 
+      // Act
+      final result = await sut.keyExists("test-key");
+
+      // Assert
+      expect(result, true);
+    });
+
+    test("keyExists returns false", () async {
+      // Arrange
+
+      // Act
+      final result = await sut.keyExists("test-key");
+
+      // Assert
+      expect(result, false);
+    });
   });
 
   group("getKey method tests", () {
+    test("key exists", () async {
+      // Arrange
+      await secureStorageFake.write(key: "test-key", value: "test-value");
 
+      // Act
+      final result = await sut.getKey("test-key");
+
+      // Assert
+      expect(result, "test-value");
+    });
+
+    test("key doesn't exist", () async {
+      // Arrange
+
+      // Act
+      final result = await sut.getKey("test-key");
+
+      // Assert
+      expect(result, "");
+    });
   });
 
   group("setKey method tests", () {
+    test("key exists after setting", () async {
+      // Arrange
 
+      // Act
+      sut.setKey("test-key", "test-value");
+      final result = await secureStorageFake.read(key: "test-key");
+
+      // Assert
+      expect(result, "test-value");
+    });
   });
 
   group("deleteKey method tests", () {
+    test("key no longer exists", () async {
+      // Arrange
+      await secureStorageFake.write(key: "test-key", value: "test-value");
 
+      // Act
+      sut.deleteKey("test-key");
+      final result = await secureStorageFake.read(key: "test-key");
+
+      // Assert
+      expect(result, null);
+    });
   });
-
-  // test("keyExists returns true", () async {
-  //
-  // });
-  //
-  // test("keyExists returns false", () async {
-  //
-  // });
 }
