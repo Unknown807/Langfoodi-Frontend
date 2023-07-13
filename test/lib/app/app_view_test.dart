473ffde377693de9094177bdafcb12aa1c5cd052
Unknown807 <milovangveric@gmail.com>
2023-07-13 16:04:10 +0100

    Added widget tests for app_view.dart

---
+++ test/lib/app/app_view_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:recipe_social_media/app/app_view.dart';
+import 'package:recipe_social_media/pages/home/home_page.dart';
+import 'package:recipe_social_media/pages/login/login_page.dart';
+import 'package:recipe_social_media/pages/splash/splash_page.dart';
+import '../../mocks/generic_mocks.dart';
+
+void main() {
+  late AuthenticationRepositoryMock authRepoMock;
+  late NavigationRepositoryMock navigRepoMock;
+
+  setUp(() {
+    authRepoMock = AuthenticationRepositoryMock();
+    navigRepoMock = NavigationRepositoryMock();
+  });
+
+  Widget createWidgetUnderTest() {
+    return App(authRepo: authRepoMock, navigationRepo: navigRepoMock);
+  }
+
+  testWidgets("app status is authenticated", (widgetTester) async {
+    // Arrange
+    when(() => authRepoMock.isAuthenticated()).thenAnswer((invocation) => Future.value(true));
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+    await widgetTester.pumpAndSettle();
+
+    // Assert
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+
+  testWidgets("app status is unauthenticated", (widgetTester) async {
+    // Arrange
+    when(() => authRepoMock.isAuthenticated()).thenAnswer((invocation) => Future.value(false));
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+    await widgetTester.pumpAndSettle();
+
+    // Assert
+    expect(find.byType(LoginPage), findsOneWidget);
+  });
+
+  testWidgets("app status is loading", (widgetTester) async {
+    // Arrange
+    when(() => authRepoMock.isAuthenticated()).thenAnswer((invocation) => Future.delayed(const Duration(seconds: 5), () => Future.value(false)));
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+
+    // Assert
+    expect(find.byType(SplashPage), findsOneWidget);
+    await widgetTester.pumpAndSettle();
+  });
+}
--- test/mocks/generic_mocks.dart
+++ test/mocks/generic_mocks.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:recipe_social_media/repositories/authentication/auth_repo.dart';
+import 'package:recipe_social_media/repositories/navigation/navigation_repo.dart';
 import 'package:recipe_social_media/utilities/utilities.dart';
 
 class ResponseMock extends Mock implements Response {}
@@ -10,3 +11,5 @@ class ClientMock extends Mock implements Client {}
 class JsonWrapperMock extends Mock implements JsonWrapper {}
 
 class AuthenticationRepositoryMock extends Mock implements AuthenticationRepository {}
+
+class NavigationRepositoryMock extends Mock implements NavigationRepository {}
