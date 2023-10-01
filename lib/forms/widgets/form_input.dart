f1aef2f9c3d1f958327da1426e90d9e5c18ee348
mg699 <mg699@kent.ac.uk>
2023-10-01 19:13:14 +0100

    Added some initial creation page design and added some more design options in the form input widget

--- lib/forms/widgets/form_input.dart
+++ lib/forms/widgets/form_input.dart
@@ -4,28 +4,45 @@ class FormInput extends StatelessWidget {
   FormInput(
       {super.key,
       this.errorText,
+      this.width,
+      this.textColor,
+      this.fontWeight,
+      this.fontSize,
       required this.hint,
       required this.eventFunc,
       this.useBorderStyle = true,
-      this.isConfidential = false});
+      this.isConfidential = false,
+      this.textAlign = TextAlign.left});
 
   String? errorText;
+  double? width;
+  Color? textColor;
+  FontWeight? fontWeight;
+  double? fontSize;
   bool isConfidential;
   bool useBorderStyle;
+  TextAlign textAlign;
   final String hint;
   final Function eventFunc;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        width: width,
         padding: const EdgeInsets.all(8.0),
         decoration: useBorderStyle
             ? BoxDecoration(
                 border: Border(bottom: BorderSide(color: Colors.grey.shade100)))
             : null,
         child: TextField(
+          textAlign: textAlign,
           obscureText: isConfidential,
           onChanged: (value) => eventFunc(value),
+          style: TextStyle(
+              color: textColor,
+              fontWeight: fontWeight,
+              fontSize: fontSize
+          ),
           decoration: InputDecoration(
               border: InputBorder.none,
               hintText: hint,
--- lib/pages/recipes/recipe_creation/recipe_creation_page.dart
+++ lib/pages/recipes/recipe_creation/recipe_creation_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_social_media/forms/widgets/form_widgets.dart';
 
 class RecipeCreationPage extends StatelessWidget {
   const RecipeCreationPage({super.key});
@@ -6,8 +7,48 @@ class RecipeCreationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Text("creation page"),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_circle_left_outlined,
+                  color: Colors.indigoAccent,
+                  size: 30,
+                ),
+                onPressed: () {},
+              ),
+              const Spacer(),
+              IconButton(
+                padding: const EdgeInsets.only(top: 5.0, right: 10.0),
+                icon: const Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.lightGreenAccent,
+                  size: 30,
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                FormInput(
+                  hint: "Recipe Name Here",
+                  useBorderStyle: true,
+                  width: MediaQuery.of(context).size.width / 1.25,
+                  textAlign: TextAlign.center,
+                  fontSize: 20,
+                  eventFunc: (val) {})
+              ],
+            )
+          )
+        ],
+      ),
     );
   }
 }
