0d8a796281ce92dd10071f6fe7e9a9cc48fe9947
ff235 <ff235@kent.ac.uk>
2023-10-02 19:02:20 +0100

    Added menu item for editing a user's bio.

--- lib/pages/profile_settings/profile_settings_page.dart
+++ lib/pages/profile_settings/profile_settings_page.dart
@@ -3,15 +3,16 @@ import 'package:recipe_social_media/widgets/shared_widgets.dart';
 
 class ProfileSettingsPage extends StatelessWidget {
   const ProfileSettingsPage({super.key});
-//TODO: Add profile image, back button, edit bio text box with character limit
+//TODO: Add profile image, back button
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:Text("Profile settings")),
       body: ListView(
-          children:  [
+          children:  const [
             CustomMenuItem(titleText:"Username",subtitleText:"Cool guy"),
             CustomMenuItem(titleText:"Email",subtitleText:"me@cooldude.com"),
+            CustomMenuItem(titleText:"Bio",subtitleText:"I like cooking"),
             CustomMenuItem(titleText:"Password",subtitleText:null)
           ]
       ),
