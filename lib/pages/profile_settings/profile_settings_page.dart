75736ee3f0596272d75066f13f1247eeadef61aa
ff235 <ff235@kent.ac.uk>
2023-10-08 16:06:49 +0100

    Renamed CustomMenuItem to CustomItemTile, created avatar widget and created profile setting top bar widget.

--- lib/pages/profile_settings/profile_settings_page.dart
+++ lib/pages/profile_settings/profile_settings_page.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_social_media/widgets/shared_widgets.dart';
+part 'widgets/top_app_bar.dart';
 
 class ProfileSettingsPage extends StatelessWidget {
   const ProfileSettingsPage({super.key});
-//TODO: Add profile image, back button
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("Profile settings")),
-      body: ListView(
+      appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(56),
+          child: TopAppBar(title: "Edit profile")),
+      body: Column(children:<Widget>[
+        const CustomAvatar(),
+        Expanded(child:ListView(
           children:  const [
-            CustomMenuItem(titleText:"Username",subtitleText:"Cool guy"),
-            CustomMenuItem(titleText:"Email",subtitleText:"me@cooldude.com"),
-            CustomMenuItem(titleText:"Bio",subtitleText:"I like cooking"),
-            CustomMenuItem(titleText:"Password",subtitleText:null)
+            CustomItemTile(titleText:"Username",subtitleText:"Cool guy"),
+            CustomItemTile(titleText:"Email",subtitleText:"me@cooldude.com"),
+            CustomItemTile(titleText:"Bio",subtitleText:"I like cooking"),
+            CustomItemTile(titleText:"Password",subtitleText:null)
           ]
-      ),
+      ))]),
     );
   }
 }
