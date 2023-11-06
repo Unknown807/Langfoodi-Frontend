0c003192dfa64cd246db63dea9cbedda521f0015
mg699 <mg699@kent.ac.uk>
2023-11-06 16:03:43 +0000

    Moved custom item tile to shared widgets folder and amended way parameters are passed in

--- lib/pages/profile_settings/profile_settings_page.dart
+++ lib/pages/profile_settings/profile_settings_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipe_social_media/repositories/navigation/navigation_repo.dart';
 import 'package:recipe_social_media/widgets/shared_widgets.dart';
-
-import '../../repositories/navigation/navigation_repo.dart';
 
 class ProfileSettingsPage extends StatelessWidget {
   const ProfileSettingsPage({super.key});
@@ -16,7 +15,11 @@ class ProfileSettingsPage extends StatelessWidget {
             title: const Text("Edit profile"),
             backgroundColor: Colors.green,
             leading: GestureDetector(
-              onTap: () => context.read<NavigationRepository>().goTo(context, "/home", RouteType.backLink),
+              onTap: () {
+                context
+                  .read<NavigationRepository>()
+                  .goTo(context, "/home", routeType: RouteType.backLink);
+              },
               child: const Icon(
                 Icons.arrow_back,
               ),
@@ -28,11 +31,30 @@ class ProfileSettingsPage extends StatelessWidget {
             child:CustomAvatar(size:70)),
         Expanded(child:ListView(
           children:  const [
-            CustomItemTile(titleText:"Username",subtitleText:"Cool Guy"),
-            CustomItemTile(titleText:"Handle",subtitleText:"@realcoolguy"),
-            CustomItemTile(titleText:"Bio",subtitleText:"I like cooking"),
-            CustomItemTile(titleText:"Email",subtitleText:"me@cooldude.com"),
-            CustomItemTile(titleText:"Password",subtitleText:null),
+            CustomItemTile(
+              titleWidget: Text("Username"),
+              subtitleWidget: Text("Cool Guy"),
+              trailingWidget: Text("Edit"),
+            ),
+            CustomItemTile(
+              titleWidget: Text("Handle"),
+              subtitleWidget: Text("@realcoolguy"),
+              trailingWidget: Text("Edit"),
+            ),
+            CustomItemTile(
+              titleWidget: Text("Bio"),
+              subtitleWidget: Text("I like cooking"),
+              trailingWidget: Text("Edit"),
+            ),
+            CustomItemTile(
+              titleWidget: Text("Email"),
+              subtitleWidget: Text("me@cooldude.com"),
+              trailingWidget: Text("Edit"),
+            ),
+            CustomItemTile(
+              titleWidget: Text("Password"),
+              trailingWidget: Text("Edit"),
+            )
           ]
       ))]),
     );
