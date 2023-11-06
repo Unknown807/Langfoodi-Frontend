f12f4a4320357d5ad86f8150cbca42351916a53d
mg699 <mg699@kent.ac.uk>
2023-11-06 16:11:23 +0000

    Added missed regex renaming from label to tag

--- lib/pages/recipes/recipe_interaction/models/recipe_tag.dart
+++ lib/pages/recipes/recipe_interaction/models/recipe_tag.dart
@@ -4,13 +4,13 @@ class RecipeTag extends FormzInput<String, RecipeFormValidationError> {
   const RecipeTag.pure() : super.pure("");
   const RecipeTag.dirty([super.value = ""]) : super.dirty();
 
-  static final RegExp _recipeLabelExp = RegExp(
+  static final RegExp _recipeTagExp = RegExp(
     r"^[a-zA-Z0-9]{1,20}$"
   );
 
   @override
   RecipeFormValidationError? validator(String value) {
-    return _recipeLabelExp.hasMatch(value)
+    return _recipeTagExp.hasMatch(value)
         ? null
         : RecipeFormValidationError.recipeTagInvalid;
   }
