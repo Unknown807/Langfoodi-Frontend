a27911ba54c190efe5faacdfd534c981ce34753d
mg699 <mg699@kent.ac.uk>
2023-11-06 16:06:59 +0000

    Fixed import pathing to follow convention

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,15 +5,13 @@ import 'package:http/http.dart';
 import 'package:recipe_social_media/pages/recipes/recipe_interaction/recipe_interaction_page.dart';
 import 'package:recipe_social_media/pages/recipes/recipe_view/recipe_view_page.dart';
 import 'package:recipe_social_media/repositories/authentication/auth_repo.dart';
-
 import 'package:recipe_social_media/pages/home/home_page.dart';
 import 'package:recipe_social_media/pages/login/login_page.dart';
 import 'package:recipe_social_media/pages/register/register_page.dart';
 import 'package:recipe_social_media/pages/splash/splash_page.dart';
 import 'package:recipe_social_media/repositories/navigation/navigation_repo.dart';
 import 'package:recipe_social_media/utilities/utilities.dart';
-
-import '../pages/profile_settings/profile_settings_page.dart';
+import 'package:recipe_social_media/pages/profile_settings/profile_settings_page.dart';
 
 export 'app.dart';
 
