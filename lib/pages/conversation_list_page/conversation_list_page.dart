69f2e08631dda2a8f22bb0d885658759b97abcca
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-28 18:49:49 +0000

    Added SoryBy section to the Conversation List page

--- lib/pages/conversation_list_page/conversation_list_page.dart
+++ lib/pages/conversation_list_page/conversation_list_page.dart
@@ -1,35 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_social_media/utilities/utilities.dart';
 
-class ConversationListPage extends StatelessWidget implements PageLander
+class ConversationListPage extends StatefulWidget implements PageLander
 {
   const ConversationListPage({super.key});
 
-  final int numberOfConversations = 10;
-  final Widget conversationDivider = const Divider(height:1, color: Color(0xFFeaeaea));
+  @override
+  State<ConversationListPage> createState() => _ConversationListPageState();
 
   @override
   void onLanding(BuildContext context) {
-
+    
   }
+}
+
+class _ConversationListPageState extends State<ConversationListPage> {
+  final int numberOfConversations = 10;
+  final Widget conversationDivider = const Divider(height:1, color: Color(0xFFeaeaea));
+
+  SortingOption selectedSortingOption = SortingOption.LastMessage;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            bool isAtListEnd = index == 0 || index == numberOfConversations + 1;
-            if (isAtListEnd) {
-              return const SizedBox.shrink();
-            }
-            return buildConversationCard(index);
-          },
-          separatorBuilder: (context, index) {
-            return conversationDivider;
-          },
-          itemCount: 2 + numberOfConversations),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 5, bottom: 10),
+            color: const Color(0xFF02A713),
+            child: Row(
+              children: [
+                const Text("Sort By:", style: TextStyle(color: Colors.white, fontSize: 16)),
+                const SizedBox(width: 18),
+                SortingOptionButton(
+                  isSelected: selectedSortingOption == SortingOption.LastMessage,
+                  sortingOption: SortingOption.LastMessage,
+                    onPressed: ()
+                    {
+                      setState(() {
+                        selectedSortingOption = SortingOption.LastMessage;
+                      });
+                    }
+                ),
+                const SizedBox(width: 18),
+                SortingOptionButton(
+                  isSelected: selectedSortingOption == SortingOption.AlphabeticalOrder,
+                  sortingOption: SortingOption.AlphabeticalOrder,
+                  onPressed: ()
+                  {
+                    setState(() {
+                      selectedSortingOption = SortingOption.AlphabeticalOrder;
+                    });
+                  }
+                ),
+                const SizedBox(width: 18),
+                SortingOptionButton(
+                  isSelected: selectedSortingOption == SortingOption.DateAdded,
+                  sortingOption: SortingOption.DateAdded,
+                  onPressed: ()
+                  {
+                    setState(() {
+                      selectedSortingOption = SortingOption.DateAdded;
+                    });
+                  }
+                )
+              ]
+            )
+          ),
+          Expanded(
+            child:
+            ListView.separated(
+              itemBuilder: (context, index) {
+                bool isAtListEnd = index == 0 || index == numberOfConversations + 1;
+                if (isAtListEnd) {
+                  return const SizedBox.shrink();
+                }
+                return buildConversationCard(index);
+              },
+              separatorBuilder: (context, index) {
+                return conversationDivider;
+              },
+              itemCount: 2 + numberOfConversations
+            )
+          )
+        ]
+      ),
       floatingActionButton: const NewConversationFloatingButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
@@ -59,6 +117,57 @@ class ConversationListPage extends StatelessWidget implements PageLander
   );
 }
 
+class SortingOptionButton extends StatefulWidget{
+  final bool isSelected;
+  final SortingOption sortingOption;
+  final VoidCallback? onPressed;
+
+  const SortingOptionButton({super.key, required this.isSelected, required this.sortingOption, this.onPressed});
+
+  @override
+  State<SortingOptionButton> createState() => _SortingOptionButtonState();
+}
+
+class _SortingOptionButtonState extends State<SortingOptionButton> {
+
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onPressed,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
+          decoration: BoxDecoration(
+            color: widget.isSelected
+                ? const Color(0xFF09CE1C)
+                : isHovered ? const Color(0xFF008E14) : const Color(0xFF05E15D),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Text(
+            getSortingOptionDisplayText(widget.sortingOption),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      )
+    );
+  }
+
+  String getSortingOptionDisplayText(SortingOption sortingOption) {
+    switch (sortingOption) {
+      case SortingOption.LastMessage:
+        return 'Last message';
+      case SortingOption.AlphabeticalOrder:
+        return 'Alphabetical order';
+      case SortingOption.DateAdded:
+        return 'Date added';
+    }
+  }
+}
+
 class NewConversationFloatingButton extends StatelessWidget{
   const NewConversationFloatingButton({super.key});
 
@@ -80,3 +189,5 @@ class NewConversationFloatingButton extends StatelessWidget{
     );
   }
 }
+
+enum SortingOption { LastMessage, AlphabeticalOrder, DateAdded }
