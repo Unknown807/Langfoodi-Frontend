b23f0c64c2cb7173556c1878b52de9e7e54f2a3e
mg699 <mg699@kent.ac.uk>
2023-12-24 13:22:49 +0000

    Added widget tests for custom icon tile and unit tests for the request.dart put method

---
+++ test/lib/widgets/custom_icon_tile_test.dart
@@ -0,0 +1,23 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_social_media/widgets/shared_widgets.dart';
+
+void main() {
+  Widget createWidgetUnderTest() {
+    return const MaterialApp(
+      home: Scaffold(
+        body: CustomIconTile(icon: Icons.abc_sharp, tileColor: Colors.black),
+      )
+    );
+  }
+
+  testWidgets("(Custom Icon Tile) dotted border with icon visible", (widgetTester) async {
+    // Arrange
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+
+    // Assert
+    expect(find.byType(DottedBorder), findsOneWidget);
+    expect(find.byIcon(Icons.abc_sharp), findsOneWidget);
+  });
+}
