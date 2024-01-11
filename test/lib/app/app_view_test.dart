e568fb42c1680b762c040fb21a182fd849079b9a
mg699 <mg699@kent.ac.uk>
2024-01-11 20:09:40 +0000

    Fixed failing home page and app view tests

--- test/lib/app/app_view_test.dart
+++ test/lib/app/app_view_test.dart
@@ -5,17 +5,19 @@ import 'package:recipe_social_media/app/app.dart';
 import 'package:recipe_social_media/pages/home/home_page.dart';
 import 'package:recipe_social_media/pages/login/login_page.dart';
 import 'package:recipe_social_media/pages/splash/splash_page.dart';
-import 'package:recipe_social_media/repositories/recipe/recipe_repo.dart';
 import '../../../test_utilities/mocks/generic_mocks.dart';
 
 void main() {
   late AuthenticationRepositoryMock authRepoMock;
   late NavigationRepositoryMock navigRepoMock;
+  late RecipeRepositoryMock recipeRepoMock;
+  late ImageRepositoryMock imageRepoMock;
   late ImageTransformationBuilderMock imageTransformationBuilderMock;
   late ImageBuilderMock imageBuilderMock;
 
   setUp(() {
-    RecipeRepository(RequestMock(), JsonWrapperMock());
+    recipeRepoMock = RecipeRepositoryMock();
+    imageRepoMock = ImageRepositoryMock();
     authRepoMock = AuthenticationRepositoryMock();
     navigRepoMock = NavigationRepositoryMock();
     imageBuilderMock = ImageBuilderMock();
@@ -26,6 +28,8 @@ void main() {
     return App(
       authRepo: authRepoMock,
       navigationRepo: navigRepoMock,
+      imageRepo: imageRepoMock,
+      recipeRepo: recipeRepoMock,
       imageBuilder: imageBuilderMock,
       imageTransformationBuilder: imageTransformationBuilderMock,
     );
