cb84b225c2053f3c4b059180c2fed99cd84e9c66
mg699 <mg699@kent.ac.uk>
2024-01-18 12:36:32 +0000

    Fixing failing unit/widget tests (WIP)

--- test/lib/app/bloc/app_bloc_test.dart
+++ test/lib/app/bloc/app_bloc_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:recipe_social_media/app/app.dart';
@@ -6,26 +7,49 @@ import '../../../../test_utilities/mocks/generic_mocks.dart';
 
 void main() {
   late AuthenticationRepositoryMock authRepoMock;
+  late LocalStoreMock localStoreMock;
 
   setUp(() {
     authRepoMock = AuthenticationRepositoryMock();
+    localStoreMock = LocalStoreMock();
   });
 
-  group("_initializeState method tests", () {
-    blocTest("isAuthenticated is true",
+  group("_changeAppTheme method tests", () {
+    blocTest("new theme is set and emitted",
+      build: () => AppBloc(localStoreMock, authRepoMock),
+      act: (bloc) => bloc.add(const ChangeAppTheme(ThemeMode.dark)),
+      expect: () => [
+        const AppState(themeMode: ThemeMode.dark)
+      ],
+      verify: (_) {
+        verify(() => localStoreMock.setKey("currentTheme", "dark")).called(1);
+      }
+    );
+  });
+
+  group("_initState method tests", () {
+    blocTest("isAuthenticated is true and theme is light mode",
       build: () {
         when(() => authRepoMock.isAuthenticated()).thenAnswer((invocation) => Future.value(true));
-        return AppBloc(authRepo: authRepoMock);
+        when(() => localStoreMock.getKey("currentTheme")).thenAnswer((invocation) => Future.value(null));
+        return AppBloc(localStoreMock, authRepoMock);
       },
-      expect: () => [const AppState.authenticated()]
+      act: (bloc) => bloc.add(const InitState()),
+      expect: () => [
+        const AppState(status: AppStatus.authenticated, themeMode: ThemeMode.light)
+      ]
     );
 
-    blocTest("isAuthenticated is false",
+    blocTest("isAuthenticated is false and theme is dark mode",
       build: () {
         when(() => authRepoMock.isAuthenticated()).thenAnswer((invocation) => Future.value(false));
-        return AppBloc(authRepo: authRepoMock);
+        when(() => localStoreMock.getKey("currentTheme")).thenAnswer((invocation) => Future.value("dark"));
+        return AppBloc(localStoreMock, authRepoMock);
       },
-      expect: () => [const AppState.unauthenticated()]
+      act: (bloc) => bloc.add(const InitState()),
+      expect: () => [
+        const AppState(status: AppStatus.unauthenticated, themeMode: ThemeMode.dark)
+      ]
     );
   });
 }
